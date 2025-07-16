5c4635e5621b593675a65bbc3dfe91f7def5a8bd
krawwwwy <krakenlex@mail.ru>
2025-07-16 18:03:42 +0300

    sso: add new storage errors

--- services/sso/internal/storage/storage.go
+++ services/sso/internal/storage/storage.go
@@ -3,6 +3,8 @@ package storage
 import "errors"
 
 var (
-	ErrUserExists   = errors.New("user already exists")
-	ErrUserNotFound = errors.New("user is not found")
+	ErrUserExists          = errors.New("user already exists")
+	ErrUserNotFound        = errors.New("user is not found")
+	ErrUserDontHasThisRole = errors.New("user don't have this role")
+	ErrUserHasAnotherRole  = errors.New("user has an account with another role")
 )
